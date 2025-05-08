a4536a8e08aa195b25fcfe56836fe2265e003699
MUHAMMED NIHAL <muhammednihall@proton.me>
2025-05-08 12:50:10 +0530

    Add files via upload

---
+++ prime.sh
@@ -0,0 +1,18 @@
+if [ $# -ne 1 ]; then
+echo "Syntax Error: $0 <number>"
+else
+if [ $1 -le 1 ]; then
+echo "Its not a prime number"
+exit 1
+fi
+i=2
+while [ $i -le `expr $1 / 2` ]; do
+if [ `expr $1 % $i` -eq 0 ]; then
+echo "$1  not a prime number" 
+exit 0
+fi
+i=`expr $i + 1`
+done
+echo "$1 is a prime number"
+fi
+
