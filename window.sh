08ceba1974fd4d05e74e00fcb7381b2eb843152b
MUHAMMED NIHAL <muhammednihall@proton.me>
2025-05-22 12:40:30 +0530

    Add files via upload

---
+++ window.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 0 ]; then
+echo "Syntax error"
+exit 1
+fi
+echo -e "\n\nDirectory of `pwd`\n"
+for f in *; do
+dt=`date -r $f "+%d-%m-%Y   %H:%M:%S"`  
+    if [ -d "$f" ]; then
+        echo "$dt 	<DIR>   $f"
+    else
+        fs=`cat $f | wc -c`
+        echo "$dt 	$fs   $f"
+    fi
+done
